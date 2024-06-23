c8ab30bf762eda42ce543280091446e0dc20288e
juri turiano <juricuber@gmail.com>
2024-06-23 17:24:38 +0800

    udjestments

--- src/components/Login-form.jsx
+++ src/components/Login-form.jsx
@@ -9,33 +9,30 @@ export const RegForm = () => {
         <form className="w-[60%] mx-auto">
           <div className="flex flex-col my-6">
             <div className="mb-4 relative flex items-center">
-              <UserIcon className="w-4 h-4 mr-2 text-gray-400" />
               <input
                 id="username"
-                className="border-[1px] border-slate-600 rounded-md p-[4px] w-full text-gray-700"
+                className="border-[1px] border-slate-600 rounded-md p-[4px] w-full text-gray-700 pr-10" /* Add padding-right to make space for the icon */
                 type="text"
                 placeholder="Username"
               />
+              <UserIcon className="w-4 h-4 text-gray-400 absolute right-2" />
             </div>
             <div className="mb-2 relative flex items-center">
-              <PassIcon className="w-4 h-4 mr-2 text-gray-400" />
               <input
                 id="password"
-                className="border-[1px] border-slate-600 rounded-md p-[4px] w-full text-gray-700"
+                className="border-[1px] border-slate-600 rounded-md p-[4px] w-full text-gray-700 pr-10" /* Add padding-right to make space for the icon */
                 type="password"
                 placeholder="Password"
               />
+              <PassIcon className="w-4 h-4 text-gray-400 absolute right-2" />
             </div>
-            <a
-              className="text-slate-400 text-[14px] ml-5 hover:underline"
-              href="#"
-            >
+            <a className="text-slate-400 text-[14px] hover:underline" href="#">
               Forget password?
             </a>
           </div>
           <div className="flex justify-center">
             <button
-              className="text-white bg-[#121212] w-[80%] text-center px-4 py-1 flex items-center justify-center rounded-md cursor-pointer hover:bg-[#030149]"
+              className="text-white bg-[#121212] w-[100%] text-center px-4 py-1 flex items-center justify-center rounded-md cursor-pointer hover:bg-[#030149]"
               type="submit"
               value="Log in"
             >
