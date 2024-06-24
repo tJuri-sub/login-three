ce013cac150c7d738a4291090f58592760bbaf2e
juri turiano <juricuber@gmail.com>
2024-06-24 21:04:58 +0800

    image adjust

--- src/components/Login-form.jsx
+++ src/components/Login-form.jsx
@@ -46,7 +46,7 @@ export const RegForm = () => {
       <div className="bg-gradient-to-r from-[#121212] to-[#030149] w-[50%] p-3 flex flex-col justify-center items-center rounded-r-lg">
         <img
           className="w-[30%] rounded-2xl shadow-lg mb-3"
-          src="/logo.jpg"
+          src="/logo.png"
           alt="Logo VigiWheel"
         />
         <h1 className="text-3xl font-bold text-white tracking-wider">
