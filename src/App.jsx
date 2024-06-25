dafe6f70ba69deae37480d604f9c837615bc759a
juri turiano <juricuber@gmail.com>
2024-06-25 08:09:19 +0800

    change bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { RegForm } from "./components/Login-form";
+import { IconCam } from "./components/icons";
 
 function App() {
   return (
@@ -6,6 +7,11 @@ function App() {
       <div className="h-screen flex justify-center items-center ">
         <div>
           <RegForm />
+          <div className="-z-10 absolute bottom-0 left-0">
+            <IconCam
+              style={{ opacity: 0.2, transform: "translate(-30%, 40%)" }}
+            />
+          </div>
         </div>
       </div>
     </>
