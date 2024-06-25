da962e5b4a24e088381e540366c5402e3061d9e5
juri turiano <juricuber@gmail.com>
2024-06-25 09:47:21 +0800

    color correction

--- src/components/Login-form.jsx
+++ src/components/Login-form.jsx
@@ -5,7 +5,7 @@ export const RegForm = () => {
     <div className="w-[50%] mx-auto bg-white min-h-[400px] rounded-lg flex shadowbox ">
       {/* Left Side: Login Form */}
       <div className="w-[50%] p-3 flex flex-col justify-center">
-        <h1 className="text-center text-2xl mb-4">Login</h1>
+        <h1 className="text-center text-2xl mb-4">Welcome!</h1>
         <form className="w-[60%] mx-auto">
           <div className="flex flex-col my-6">
             <div className="mb-4 relative flex items-center">
@@ -27,23 +27,23 @@ export const RegForm = () => {
               <PassIcon className="w-4 h-4 text-gray-400 absolute right-2" />
             </div>
             <a className="text-slate-400 text-[14px] hover:underline" href="#">
-              Forget password?
+              Forgot password?
             </a>
           </div>
           <div className="flex justify-center">
             <button
-              className="text-white bg-[#121212] w-[100%] text-center px-4 py-1 flex items-center justify-center rounded-md cursor-pointer hover:bg-[#030149]"
+              className="text-white bg-[#030149] w-[100%] text-center px-4 py-1 flex items-center justify-center rounded-md cursor-pointer hover:bg-[#1b2374]"
               type="submit"
-              value="Log in"
             >
-              Log in <LoginIcon className="ml-2" />
+              Log in
+              {/* <LoginIcon className="ml-2" /> */}
             </button>
           </div>
         </form>
       </div>
 
       {/* Right Side: Logo */}
-      <div className="bg-gradient-to-r from-[#030149] to-[#121212] w-[50%] p-3 flex flex-col justify-center items-center rounded-r-lg">
+      <div className="bg-gradient-to-r from-[#04023d] to-[#0e1629] w-[50%] p-3 flex flex-col justify-center items-center rounded-r-lg">
         <img
           className="w-[30%] rounded-3xl shadow-lg mb-3"
           src="/logo.png"
